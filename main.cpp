a7273b56df32f0886ce2f8786eac4d83f67848b7
Dennis Fenchenko <denisfench@gmail.com>
2021-11-18 21:33:13 -0500

    load URLs implementation

--- main.cpp
+++ main.cpp
@@ -9,13 +9,15 @@ using namespace std;
 
 const string indexFileName = "index.bin";
 const string lexiconFileName = "lexicon.txt";
+const string testIndexFileName = "test_index.bin";
+const string urlsFileName = "test_urls.txt";
 const char comma = ',';
 const char space = ' ';
 const char tab = '\t';
 const char newline = '\n';
-unordered_map <string, tuple<int, int, int>> lexicon;
-const string testIndexFileName = "test_index.bin";
 
+unordered_map <string, tuple<int, int, int>> lexicon;
+unordered_map <string, int> URLs;
 
 template <typename T>
 void printVec(T vec);
@@ -23,13 +25,18 @@ void printVec(T vec);
 void printLexicon();
 
 ifstream indexReader(indexFileName);
+ifstream URLsInStream(urlsFileName);
 
 // declare the function prototypes
 void loadLexicon();
 
+void loadUrls();
+
 vector<char> openList(string term);
 
 vector<int> VBDecodeVec(const vector<char>& encodedData);
+
+vector<int> VBDecodeFile(string filename);
 
 int main() {
     cout << "The main begins!" << std::endl;
@@ -42,12 +49,15 @@ int main() {
 
     loadLexicon();
 
-    vector<char> invList = openList("suffered");
-    vector<int> decodedList = VBDecodeVec(invList);
-    printVec(decodedList);
+//    vector<char> invList = openList("courageous");
+//    vector<int> decodedList = VBDecodeVec(invList);
+    vector<int> decodedIndexFile = VBDecodeFile(indexFileName);
+    printVec(decodedIndexFile);
 
     // close the streams
     indexReader.close();
+    URLsInStream.close();
+
     return 0;
 }
 
@@ -141,6 +151,22 @@ void loadLexicon() {
 }
 
 
+void loadUrls() {
+
+    string line;
+    string URL;
+    int docId;
+
+    while(getline(URLsInStream, line)) {
+        stringstream lineStream(line);
+        lineStream >> URL;
+        lineStream >> docId;
+        URLs.insert(make_pair(URL, docId));
+    }
+
+}
+
+
 vector<char> openList(string term) {
     vector<int> invertedList;
     vector<char> encodedInvertedList;
@@ -209,5 +235,36 @@ void printLexicon() {
     for (auto const& entry: lexicon) {
         printTuple(entry.first, entry.second);
     }
+}
+
+
+vector<int> VBDecodeFile(string filename) {
+    ifstream ifile;
+    ifile.open(filename, ios::binary);
+    char c;
+    int num;
+    int p;
+    vector<int> result;
+    vector<char> vec = read_com(ifile);
+
+    for(vector<char>::iterator it = vec.begin(); it != vec.end(); it++){
+        c = *it;
+        bitset<8> byte(c);
+        num = 0;
+        p = 0;
+        while(byte[7] == 1){
+            byte.flip(7);
+            num += byte.to_ulong()*pow(128, p);
+            cout << "num " << num << endl;
+            p++;
+            it ++;
+            c = *it;
+            byte = bitset<8>(c);
+        }
+        num += (byte.to_ulong())*pow(128, p);
+
+        result.push_back(num);
+    }
+    return result;
 }
 
